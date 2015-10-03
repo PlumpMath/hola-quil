788a30205f48f07c31acfb392c6d47a57d3f37d0
Esperanza Moreno Cruz <hola@mi-mina.com>
2015-10-03 22:17:56 +0200

    triangulos

--- src/patterns/1_5_triángulos.clj
+++ src/patterns/1_5_triángulos.clj
@@ -1,18 +1,17 @@
 ;; La curvatura del sin depende del lado
 
 
-(ns patterns.1_4_triangulos
+(ns patterns.1_5_triangulos
   (:require [quil.core :as q]
             [quil.helpers.drawing :as d]
             [quil.helpers.seqs :as s]
             [quil.helpers.calc :as c]))
 
 
-
 (defn key-pressed []
   ;(println (q/key-code))
   (cond
-   (= 49 (q/key-code)) (q/save-frame "01_4_triangulos-####.png"))) ;1
+   (= 49 (q/key-code)) (q/save-frame "01_5_triangulos-####.png"))) ;1
 
 (defn dibuja-triangulo-equilatero [x y l]
   (let [h (* l (q/sin (q/radians 60)))
@@ -25,11 +24,13 @@
         step (/ l 36)
         n (+ 1 (/ l step))
         xs (s/range-incl 0 l step)
-        rads (map #(* 10 (q/radians %)) (range n))
-        ys (map q/sin rads)
+        ys (repeat (inc (/ l step))  0)
+        line-args (d/line-join-points xs ys)]
+        ;rads (map #(* 10 (q/radians %)) (range n))
+        ;ys  (map q/sin rads)
         ;yss (map #(Math/pow % 3) ys)
-        scaled-ys (map #(* (/ l 10) %) ys)
-        line-args (d/line-join-points xs scaled-ys)]
+        ;scaled-ys (map #(* (/ l 10) %) ys)
+        ;line-args (d/line-join-points xs scaled-ys)]
 
     (q/with-translation [x1 y1]
       (q/rotate (q/radians 60))
@@ -41,6 +42,14 @@
       (q/rotate (q/radians 180))
       (dorun (map #(apply q/line %) line-args)))))
 
+(Math/asin 0)
+(Math/asin 0.5)
+(Math/asin 1) ;; PI/2
+(Math/asin -0.5)
+
+(def a q/PI)
+a
+
 (defn setup []
   (q/color-mode :hsb 360 100 100)
   (q/smooth))
@@ -48,7 +57,7 @@
 (defn draw []
   (q/background 160 80 100)
   (q/stroke 360)
-  (q/stroke-weight 5)
+  (q/stroke-weight 4)
   (q/no-fill)
 
   (let [w (q/width)
@@ -57,19 +66,31 @@
         ny (* nx (q/sin (/ q/PI 3)))
         l (if (= 0 (q/mouse-x)) 0.01 (q/map-range (q/mouse-x) 0 w 0 300))
         a (* l (q/sin (q/radians 60)))]
-    (doseq [x (s/range-incl 0 w  nx)
-            [index y] (s/indexed-range-incl 0 h ny)]
-        (cond
-         (odd? index)  (do
-                         (q/with-translation [x y]
-                           (q/rotate (q/map-range (q/mouse-y) 0 h 0 q/TWO-PI))
-                           (dibuja-triangulo-equilatero 0 0 l)))
-         (even? index)  (do
-                          (q/with-translation [(+ x (/ nx 2)) y]
-                           (q/rotate (q/map-range (q/mouse-y) 0 h 0 q/TWO-PI))
-                           (dibuja-triangulo-equilatero 0 0 l))))))
+    (doseq [ x         (s/range-incl 0 w  nx)
+            [index y]  (s/indexed-range-incl 0 h ny)]
+      (let [xr (cond (odd? index) x
+                     (even? index) (+ x (/ nx 2)))
+            d (q/dist xr y  (q/mouse-x)  (q/mouse-y))
+            dist (if (= 0 d) 0.0001 d)]
+        (q/with-translation [xr y]
+
+            ;(q/rotate (q/map-range (q/mouse-y) 0 h 0 q/TWO-PI))
+            ;(dibuja-triangulo-equilatero 0 0 l)
+            ;(q/text-num dist 0 0)
+            ;(q/text-num (q/mouse-x) 0 10)
+            ;(q/point 0 0)
+            (q/stroke (q/map-range xr 0 w 0 360))
+            (dibuja-triangulo-equilatero 0 0 l)
+            (q/rotate (if (> (q/mouse-y) y)
+                        (+ (/ q/PI 6) (Math/acos (/ (- (q/mouse-x) xr) dist)))
+                        (- 0 (- (Math/acos (/ (- (q/mouse-x) xr) dist)) (/ q/PI 6)))))
+            (dibuja-triangulo-equilatero 0 0 l)
+           ; (dibuja-triangulo-equilatero 0 0 (/ l 2))
+            ))
+           ))
+
   (q/fill 0)
-  (q/text "1.4.triángulos" 10 20)
+  (q/text "1.5.triángulos" 10 20)
   (q/text "mouse-x" 10 40)
   (q/text-num (q/mouse-x) 80 40)
   (q/text "mouse-y" 10 60)
